baba819c5cf3a7f57b7d3adf20a09d7b193bb2de
Asteriuz <gutocebola@gmail.com>
2024-12-10 23:01:44 -0300

    2 Try is the charm

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from 'next/server';
 
 export function middleware(request: { nextUrl: { pathname: string; }; }) {
-  if (request.nextUrl.pathname === '/Curriculo.pdf.txt') {
+  if (request.nextUrl.pathname === '/Curriculo.pdf.txt?_rsc=1wtp7') {
     return new Response(null, { status: 204 }); // Return a 204 No Content response
   }
   return NextResponse.next();
