661f051f79a3b5cd704e1c83e1469acb2fda0222
Asteriuz <gutocebola@gmail.com>
2026-03-25 18:47:54 -0300

    Configured for Cloudflare

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,6 +8,6 @@ export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
-  site: "https://asteriuz.github.io",
-  base: "/Asteriuz",
+  // site: "https://asteriuz.github.io",
+  base: "/",
 });
